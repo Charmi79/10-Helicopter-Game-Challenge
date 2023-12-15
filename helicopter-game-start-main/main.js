f97927ba45dc7d9be735760608daa091c3ac64a3
Charmi79 <145047396+Charmi79@users.noreply.github.com>
2023-12-15 10:57:07 -0700

    Add files via upload

---
+++ helicopter-game-start-main/main.js
@@ -0,0 +1,54 @@
+// Helicopter Game Start
+
+// Set up canvas and graphics context
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Global Variables
+let heliImg = document.createElement("img");
+heliImg.src = "img/heliBlueTransparent.png";
+
+let state = "start";
+let mouseIsPressed = false;
+let heli = {
+  x: 200,
+  y: 250,
+  w: 80,
+  h: 40,
+  speed: 0,
+  accel: 0.7,
+};
+
+// Draw Function
+window.addEventListener("load", draw);
+
+function draw() {
+  if (state === "start") {
+    drawStart();
+  } else if (state === "gameon") {
+    runGame();
+  } else if (state === "gameover") {
+    drawGameOver();
+  }
+
+  // Request Animation Frame
+  requestAnimationFrame(draw);
+}
+
+// EVENT STUFF
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+
+function mousedownHandler() {
+  mouseIsPressed = true;
+
+  if (state === "start") {
+    state = "gameon";
+  }
+}
+
+function mouseupHandler() {
+  mouseIsPressed = false;
+}
