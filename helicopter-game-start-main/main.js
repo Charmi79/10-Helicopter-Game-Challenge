cfdb127e6263e8f031adc83b1ceccc4ed64f4c3e
Charmi79 <145047396+Charmi79@users.noreply.github.com>
2023-12-19 11:08:12 -0700

    Update main.js

--- helicopter-game-start-main/main.js
+++ helicopter-game-start-main/main.js
@@ -18,25 +18,29 @@ propeller.src = "sound/propeller.wav";
 
 let mouseIsPressed = false;
 
+let best = 0;
+
 // Global Variables (Reset)
+let frameCount;
 let state;
 let heli;
 let wall1, wall2, wall3;
+let wallSpeed;
 reset();
 // Draw Function
 window.addEventListener("load", draw);
 
 function draw() {
-	if (state === "start") {
-		drawStart();
-	} else if (state === "gameon") {
-		runGame();
-	} else if (state === "gameover") {
-		drawGameOver();
-	}
+  if (state === "start") {
+    drawStart();
+  } else if (state === "gameon") {
+    runGame();
+  } else if (state === "gameover") {
+    drawGameOver();
+  }
 
-	// Request Animation Frame
-	requestAnimationFrame(draw);
+  // Request Animation Frame
+  requestAnimationFrame(draw);
 }
 
 // EVENT STUFF
@@ -44,18 +48,18 @@ document.addEventListener("mousedown", mousedownHandler);
 document.addEventListener("mouseup", mouseupHandler);
 
 function mousedownHandler() {
-	mouseIsPressed = true;
+  mouseIsPressed = true;
 
-	// Play propeller sound
-	propeller.currentTime = 0;
-	propeller.play();
+  // Play propeller sound
+  propeller.currentTime = 0;
+  propeller.play();
 
-	if (state === "start") {
-		state = "gameon";
-	}
+  if (state === "start") {
+    state = "gameon";
+  }
 }
 
 function mouseupHandler() {
-	mouseIsPressed = false;
-	propeller.pause();
+  mouseIsPressed = false;
+  propeller.pause();
 }
